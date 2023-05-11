a68ec7608a71f2edc9f6f25824451775a54d719d
wanglinhai <wanglinhai@ejiayou.com>
2023-05-11 23:41:36 +0800

    update

--- lab02-redis/src/main/java/com/k/lab02redis/config/RedisConfig.java
+++ lab02-redis/src/main/java/com/k/lab02redis/config/RedisConfig.java
@@ -3,10 +3,13 @@ package com.k.lab02redis.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
-import org.redisson.config.SingleServerConfig;
-import org.redisson.config.TransportMode;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+
+import java.io.IOException;
 
 /**
  * @author k 2023/5/9 23:40
@@ -15,15 +18,51 @@ import org.springframework.context.annotation.Configuration;
 public class RedisConfig {
 
 
+    /**
+     * 手动配置redis连接
+     *
+     * @return
+     */
+    // @Bean
+    // public RedissonClient redissonClient(){
+    //     Config config = new Config();
+    //     config.setTransportMode(TransportMode.NIO);
+    //     SingleServerConfig singleServerConfig = config.useSingleServer();
+    //     //可以用"rediss://"来启用SSL连接
+    //     singleServerConfig.setAddress("redis://127.0.0.1:6379");
+    //     singleServerConfig.setPassword("123456");
+    //     RedissonClient redisson = Redisson.create(config);
+    //     return redisson;
+    // }
+
+    // /**
+    //  * 配置文件 application.yaml 配置
+    //  *
+    //  * @return
+    //  */
+    // @Bean
+    // public RedissonClient redissonClient() {
+    //     return Redisson.create();
+    // }
+
+
+    /**
+     * 配置文件 redisson.yml 配置
+     *
+     * @return
+     */
     @Bean
-    public RedissonClient redissonClient(){
-        Config config = new Config();
-        config.setTransportMode(TransportMode.NIO);
-        SingleServerConfig singleServerConfig = config.useSingleServer();
-        //可以用"rediss://"来启用SSL连接
-        singleServerConfig.setAddress("redis://127.0.0.1:6379");
-        singleServerConfig.setPassword("123456");
-        RedissonClient redisson = Redisson.create(config);
-        return redisson;
+    public RedissonClient redissonClient() {
+        ResourceLoader loader = new DefaultResourceLoader();
+        Resource resource = loader.getResource("redisson.yml");
+        Config config = null;
+        try {
+            config = Config.fromYAML(resource.getInputStream());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return Redisson.create(config);
     }
+
+
 }
--- lab02-redis/src/test/java/com/k/lab02redis/Lab02RedisApplicationTests.java
+++ lab02-redis/src/test/java/com/k/lab02redis/Lab02RedisApplicationTests.java
@@ -1,11 +1,17 @@
 package com.k.lab02redis;
 
+import com.k.lab02redis.entity.UserInfo;
 import org.junit.jupiter.api.Test;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.redisson.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class Lab02RedisApplicationTests {
@@ -15,9 +21,72 @@ class Lab02RedisApplicationTests {
 
     @Test
     void contextLoads() {
-        redissonClient.getBucket("hello").set("bug");
-        String test = (String) redissonClient.getBucket("hello").get();
+        redissonClient.getBucket("hello1").set("bug");
+        String test = (String) redissonClient.getBucket("hello1").get();
         System.out.println(test);
-
     }
+
+    /**
+     * String
+     */
+    @Test
+    public void stringTest() {
+        RBucket<String> bucket = redissonClient.getBucket("lab02:string");
+        LocalDate now = LocalDate.now();
+        bucket.set(now.toString());
+        System.out.println(bucket.get());
+    }
+
+    /**
+     * list
+     */
+    @Test
+    public void listTest() {
+        RList<UserInfo> list = redissonClient.getList("lab02:list");
+        int size = list.size();
+        UserInfo userInfo = UserInfo.builder().id(size + 1).username("sz").password("s").phoneNumber("123").build();
+        list.add(userInfo);
+        System.out.println();
+        List<UserInfo> userInfos = list.get(size, 0);
+        System.out.println(userInfos);
+        List<UserInfo> userInfoss = list.range(0, -1);
+        System.out.println();
+        System.out.println(userInfoss);
+    }
+
+    /**
+     * map
+     */
+    @Test
+    public void mapTest() {
+        RMap<String, UserInfo> map = redissonClient.getMap("lab02:map");
+        int size = map.size();
+        int id = size + 1;
+        UserInfo userInfo = UserInfo.builder().id(id).username("sz").password("s").phoneNumber("123").build();
+        map.put(String.valueOf(id), userInfo);
+        System.out.println();
+        UserInfo userInfo1 = map.get(String.valueOf(id));
+        System.out.println(userInfo1);
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < size; i++) {
+            set.add(String.valueOf(i + 1));
+        }
+        Map<String, UserInfo> all = map.getAll(set);
+        System.out.println();
+        System.out.println(all);
+    }
+
+
+    /**
+     * lock
+     */
+    @Test
+    public void lockTest() {
+        RLock lock = redissonClient.getLock("lab02:lock");
+        lock.lock(10L, TimeUnit.SECONDS);
+        System.out.println("------");
+        lock.unlock();
+    }
+
+
 }
